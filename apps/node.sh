c87eeb930c427a0899fb58cac3422d35fb9ae39c
Tiago Nunes <tiagowinehouse@gmail.com>
2016-01-16 17:48:52 -0300

    Update node.sh

--- apps/node.sh
+++ apps/node.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y python-software-properties python g++ make
-wget http://https://nodejs.org/dist/v5.4.1/node-v5.4.1-linux-x64.tar.gz
+wget https://nodejs.org/dist/v5.4.1/node-v5.4.1-linux-x64.tar.gz
 sudo tar -C /usr/local --strip-components 1 -xzf node-v5.4.1-linux-x64.tar.gz
 sudo rm -rf node-v5.4.1-linux-x64.tar.gz
 sudo apt-get update
