4b61535da62840d18fb04706e7072d0d0c1506f0
Tiago Nunes <tiagowinehouse@gmail.com>
2016-01-16 17:42:08 -0300

    :ghost: :bulb:

---
+++ apps/node.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y python-software-properties python g++ make
+wget http://https://nodejs.org/dist/v5.4.1/node-v5.4.1-linux-x64.tar.gz
+sudo tar -C /usr/local --strip-components 1 -xzf node-v5.4.1-linux-x64.tar.gz
+sudo rm -rf node-v5.4.1-linux-x64.tar.gz
+sudo apt-get update
+node -v
+npm -v
