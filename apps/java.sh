260a6a98e996f09c6a2d2730f46c2645d5694a75
Tiago Nunes <tiagowinehouse@gmail.com>
2016-01-18 11:10:29 -0300

    Update Java.sh -y

--- apps/java.sh
+++ apps/java.sh
@@ -1,8 +1,8 @@
 echo "> Adicionando o repositório do programa ..."
-sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:webupd8team/java -y
 echo "> Atualizazndo o gerenciador de pacotes ..."
 sudo apt-get update
 echo "> Instalando o Java ..."
-sudo apt-get install oracle-java9-installer
+sudo apt-get install oracle-java9-installer -y
 echo "> Verificar a versão do Java instalado ..."
 java --version
