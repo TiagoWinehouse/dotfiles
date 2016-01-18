775096f3c0c8729e8a7512dc44d08434a79efc35
Tiago Nunes <tiagowinehouse@gmail.com>
2016-01-18 11:28:11 -0300

    Ajeitando o Java.sh com o novo repositório do java

--- apps/java.sh
+++ apps/java.sh
@@ -1,8 +1,10 @@
 echo "> Adicionando o repositório do programa ..."
-sudo add-apt-repository ppa:webupd8team/java
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 echo "> Atualizazndo o gerenciador de pacotes ..."
 sudo apt-get update
 echo "> Instalando o Java ..."
-sudo apt-get install oracle-java9-installer
+sudo apt-get install oracle-java9-installer -y
 echo "> Verificar a versão do Java instalado ..."
-java --version
+java -version
