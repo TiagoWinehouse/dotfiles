f312237ba7e639b1a6448d5883f4a0a2a8fe405a
Tiago Nunes <tiagowinehouse@gmail.com>
2016-05-18 22:08:11 -0300

    Update node.sh

--- apps/node.sh
+++ apps/node.sh
@@ -3,17 +3,17 @@ sudo apt-get install -y python-software-properties python g++ make
 echo "> Baixando o nodejs ..."
 case $( uname -m ) in
 x86_64)
-wget https://nodejs.org/dist/v6.2.0/node-v6.2.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v6.2.0/node-v6.2.0-linux-x64.tar.gz
 echo "> Extraindo o nodejs ..."
-sudo tar -C /usr/local --strip-components 1 -xzf node-v6.2.0-linux-x64.tar.xz
+sudo tar -C /usr/local --strip-components 1 -xzf node-v6.2.0-linux-x64.tar.gz
 echo "> Deletando o pacote de instalação ..."
-sudo rm -rf node-v6.2.0-linux-x64.tar.xz;;
+sudo rm -rf node-v6.2.0-linux-x64.tar.gz;;
 *)
-wget https://nodejs.org/dist/v6.2.0-/node-v6.2.0-linux-x32.tar.xz
+wget https://nodejs.org/dist/v6.2.0-/node-v6.2.0-linux-x32.tar.gz
 echo "> Extraindo o nodejs ..."
-sudo tar -C /usr/local --strip-components 1 -xzf node-v6.2.0-linux-x32.tar.xz
+sudo tar -C /usr/local --strip-components 1 -xzf node-v6.2.0-linux-x32.tar.gz
 echo "> Deletando o pacote de instalação ..."
-sudo rm -rf node-v6.2.0-linux-x32.tar.xz;;
+sudo rm -rf node-v6.2.0-linux-x32.tar.gz;;
 esac
 echo "> Atualizando ..."
 sudo apt-get update
